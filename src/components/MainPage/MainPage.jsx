eb302d54ca133ac0665ed8eda048eb68acbabc55
abdurrashidkhan <rashidkhan.destop@gmail.com>
2025-01-09 13:18:20 +0600

    update here

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -3,7 +3,7 @@ import MainContent from "../MainContent/MainContent";
 export default function MainPage() {
   return (
     <main>
-      <div className="container mx-auto px-2 ">
+      <div className="container mx-auto ">
         {/* main content */}
         <MainContent />
         {/* medici all details */}
